748240594167ed4eca65d8587cb0e103b1d35df4
TimSheard <sheard@pdx.edu>
2023-04-07 14:46:02 -0700

    Replaced Unique with RelLens, Add additional Solver cases. Added PairSpec (whose meaning is a Transformer) Added Member, NotMember, and MapMember Pred Added module Test.Cardano.Ledger.Constrained.Scripts Added Preds directory and stages for PParams, Universes, TxBody. Authored by Tim Sheard and Aniket Deshpande

--- libs/cardano-ledger-test/src/Test/Cardano/Ledger/Generic/PrettyCore.hs
+++ libs/cardano-ledger-test/src/Test/Cardano/Ledger/Generic/PrettyCore.hs
@@ -1447,7 +1447,7 @@ pcTxBodyField proof x = case x of
   WppHash (SJust h) -> [("integrity hash", trim (ppSafeHash h))]
   AdHash SNothing -> []
   AdHash (SJust (AuxiliaryDataHash h)) -> [("aux data hash", trim (ppSafeHash h))]
-  Txnetworkid SNothing -> []
+  Txnetworkid SNothing -> [("network id", ppString "Nothing")]
   Txnetworkid (SJust nid) -> [("network id", pcNetwork nid)]
   GovernanceProcs ga -> [("governance procedures", ppStrictSeq prettyA ga)]
 
@@ -1621,6 +1621,8 @@ pcPParamsSynopsis p x = withEraPParams p help
         [ ("maxBBSize", ppNatural (x ^. Core.ppMaxBBSizeL))
         , ("maxBHSize", ppNatural (x ^. Core.ppMaxBHSizeL))
         , ("maxTxSize", ppNatural (x ^. Core.ppMaxTxSizeL))
+        , ("poolDeposit", pcCoin (x ^. Core.ppPoolDepositL))
+        , ("keyDeposit", pcCoin (x ^. Core.ppKeyDepositL))
         , ("protVer", ppString (showProtver (x ^. Core.ppProtocolVersionL)))
         ]
 
